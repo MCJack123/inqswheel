50909b784861aa1d648901205af66c47b3c63d13
Jack Bruienne <jackbruienne@yahoo.com>
2018-05-26 14:55:03 -0400

    Initial Commit

---
+++ inqswheel/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  inqswheel
+//
+//  Created by Jack on 5/26/18.
+//  Copyright © 2018 JackMacWindows. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
