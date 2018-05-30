71ddd20c6301624ea61ca9152367afc3688a484a
JackMacWindows <jackbruienne@yahoo.com>
2018-05-30 11:44:29 -0400

    Started writing JS port

--- inqswheel/inqswheel.js
+++ inqswheel/inqswheel.js
@@ -1,45 +1,140 @@
-typedef struct key {
-    bool encryptable;
-    char character;
-    char values[4];
+// This requires ES6+ to work
+function key() {
+    this.encryptable = false;
+    this.character = "";
+    this.values = new Array(4);
     // values[0] = 1->26, values[1] = 27->52, values[2] = 53->78, values[3] = 79->104
-} key;
+}
 
-inline char addWithMax(char num1, char num2, char max, char min = 0) {
+function addWithMax(num1, num2, max, min = 0) {
     if (num1 + num2 > max) return min + ((num1 + num2) - max) - 1;
     else if (num1 + num2 < min) return max - (min - (num1 + num2)) + 1;
     else return num1 + num2;
 }
 
-char setcase(char c, bool lowercase) {
-    if (c >= 'A' && c <= 'Z' && lowercase) return c + ('a' - 'A');
-    else if (c >= 'a' && c <= 'z' && !lowercase) return c - ('a' - 'A');
-    else if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) return c;
-    else return c | 0b10000000;
+function co(s) {return s.charCodeAt();}
+function st(c) {return String.fromCharCode(c);}
+
+function setcase(c, lowercase) {
+    if (co(c) >= co('A') && co(c) <= co('Z') && lowercase) return st(co(c) + (co('a') - co('A')));
+    else if (co(c) >= co('a') && co(c) <= co('z') && !lowercase) return st(co(c) - (co('a') - co('A')));
+    else if ((co(c) >= co('A') && co(c) <= co('Z')) || (co(c) >= co('a') && co(c) <= co('z'))) return c;
+    else return st(co(c) | co(0b10000000));
 }
 
-key fillValue(key value, key keyval) {
-    key retval;
+function fillValue(value, keyval) {
+    var retval = key();
     if (!value.encryptable) {
-        char mainvalue = (value.character != 0 ? value.character : value.values[0]);
-        retval.character = mainvalue;
+        var mainvalue = (value.character != "" ? co(value.character) : value.values[0]);
+        retval.character = st(mainvalue);
         for (int i = 0; i < 4; i++) retval.values[i] = mainvalue;
     } else {
-            if (value.character != 0) {
+        if (value.character != "") {
             //encrypt
             retval.character = value.character;
-            char rotation = value.character - keyval.character;
-            for (int i = 0; i < 4; i++) retval.values[i] = addWithMax(keyval.values[i], rotation, 26*(i+1), 26*i + 1);
+            var rotation = co(value.character) - co(keyval.character);
+            for (var i = 0; i < 4; i++) retval.values[i] = addWithMax(keyval.values[i], rotation, 26*(i+1), 26*i + 1);
         } else {
             //decrypt
-            int wheelnum = 0;
+            var wheelnum = 0;
             if (value.values[0] > 26 && value.values[0] < 53) wheelnum = 1;
             else if (value.values[0] > 52 && value.values[0] < 79) wheelnum = 2;
             else if (value.values[0] > 78) wheelnum = 3;
-            char rotation = value.values[0] - keyval.values[wheelnum];
+            var rotation = value.values[0] - keyval.values[wheelnum];
             for (int i = 0; i < 4; i++) retval.values[i] = addWithMax(keyval.values[i], rotation, 26*(i+1), 26*i);
-            retval.character = addWithMax(keyval.character, rotation, (keyval.character < 'a' ? 'Z' : 'z'), (keyval.character < 'a' ? 'A' : 'a'));
+            retval.character = st(addWithMax(co(keyval.character), rotation, (co(keyval.character) < 'a' ? 'Z' : 'z'), (co(keyval.character) < 'a' ? 'A' : 'a')));
         }
     }
     return retval;
+}
+
+function inqswheel(encstate, keystr, text) {
+    if (keystr.length != 9) {
+        alert("Invalid key format (key format example: A01275379)");
+        return 2;
+    }
+    bool lowercase;
+    if (encstate > 2) return 3;
+    
+    var keyval = new key();
+    keyval.character = keystr[0];
+    if (co(keyval.character) >= co('A') && co(keyval.character) <= co('Z')) lowercase = false;
+    else if (co(keyval.character) >= co('a') && co(keyval.character) <= co('z')) lowercase = true;
+    else {
+        alert("Invalid key format (key format example: A01275379)\n");
+        return 2;
+    }
+    try {
+        keyval.values[0] = std::stoi(keystr.substr(1, 2));
+        keyval.values[1] = std::stoi(keystr.substr(3, 2));
+        keyval.values[2] = std::stoi(keystr.substr(5, 2));
+    } catch (std::invalid_argument &e) {
+        std::cerr << "Invalid key format (key format example: A01275379)\n";
+        return 2;
+    }
+    try {
+        keyval.values[3] = std::stoi(keystr.substr(7, 2));
+    } catch (std::invalid_argument &e) {
+        if (keystr.substr(7, 2) == "V1") keyval.values[3] = 101;
+        else if (keystr.substr(7, 2) == "V2") keyval.values[3] = 102;
+        else if (keystr.substr(7, 2) == "V3") keyval.values[3] = 103;
+        else if (keystr.substr(7, 2) == "IQ") keyval.values[3] = 104;
+        else {
+            std::cerr << "Invalid key format (key format example: A01275379)\n";
+            return 2;
+        }
+    }
+    if (keyval.values[0] > 26) {
+        std::cerr << "Invalid key format (key format example: A01275379)\n";
+        return 2;
+    }
+    if (keyval.values[1] < 27 || keyval.values[1] > 52) {
+        std::cerr << "Invalid key format (key format example: A01275379)\n";
+        return 2;
+    }
+    if (keyval.values[2] < 53 || keyval.values[2] > 78) {
+        std::cerr << "Invalid key format (key format example: A01275379)\n";
+        return 2;
+    }
+    if (keyval.values[3] < 79 || keyval.values[3] > 104) {
+        std::cerr << "Invalid key format (key format example: A01275379)\n";
+        return 2;
+    }
+    if (keyval.values[3] == 0) keyval.values[3] = 100;
+    
+    std::istream * in = getFile(argc, argv);
+    srand((unsigned)time(0));
+    while (!in->eof()) {
+        key value;
+        if (encstate == 0) {
+            //decrypt
+            char ch = in->get();
+            value.encryptable = !(ch & 0b10000000);
+            value.values[0] = ch & 0b01111111;
+            std::cout << fillValue(value, keyval).character;
+        } else {
+            //encrypt
+            char c = in->get();
+            if (c == 0x7f || c  == (-118 & 0x7f)) continue;
+            char ch = setcase(c, lowercase);
+            value.encryptable = !(ch & 0b10000000);
+            value.character = ch;
+            if (ch == -1) continue;
+            int num = rand() % 4;
+            assert(num >= 0 && num < 4);
+            char values[4];
+            strlcpy(values, fillValue(value, keyval).values, 4);
+            if (values[0] > 100) values[0] = values[1];
+            if (values[1] > 100) values[1] = values[2];
+            if (values[2] > 100) values[2] = values[0];
+            if (values[3] > 100) num = rand() % 3;
+            if (encstate == 1) std::cout << fillValue(value, keyval).values[num];
+            else std::cout << (int)fillValue(value, keyval).values[num] << " ";
+        }
+    }
+    
+    if (encstate != 1) std::cout << "\n";
+    std::cout.flush();
+    if (infile.is_open()) infile.close();
+    return 0;
 }
